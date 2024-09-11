786ea65806dbdbb2662dcc64c3c24e44f2b2bdb6
Jesse Latimer <jesselatimer@MacBook-Pro.local>
2024-09-10 19:20:14 -0700

    parallelize fetches

--- src/utils/importData.ts
+++ src/utils/importData.ts
@@ -27,98 +27,92 @@ export type DataByKey = {
 };
 
 export const getDataFromImport = async (): Promise<DataByKey> => {
-  const categoriesByKey: Record<string, Category> = await parseCsv<
-    typeof CategorySchema
-  >(
-    '/incursion/imported/Categories 209902f319f64b48bc9cca434d7e449b_all.csv',
-    CategorySchema,
-    (rowCategory) => {
-      return {
-        ...rowCategory,
-        subCategories:
-          rowCategory.subCategories &&
-          typeof rowCategory.subCategories === 'string'
-            ? rowCategory.subCategories.split(',')
-            : [],
-        appendices:
-          rowCategory.appendices && typeof rowCategory.appendices === 'string'
-            ? rowCategory.appendices.split(',')
-            : [],
-      };
-    }
-  );
-
-  const entitiesByKey: Record<EntityKey, Entity> = await parseCsv<
-    typeof EntitySchema
-  >(
-    '/incursion/imported/Entities ec7305ac1c814f7290ef03a1b62b6549_all.csv',
-    EntitySchema,
-    (rowEntity) => {
-      return {
-        ...rowEntity,
-        imageUrl: `/incursion/imported/${rowEntity.Image}`,
-        entityLevels:
-          rowEntity.entityLevels && typeof rowEntity.entityLevels === 'string'
-            ? rowEntity.entityLevels.split(',')
-            : [],
-      };
-    }
-  );
-
-  const pointTypesByKey: Record<PointTypeKey, PointType> = await parseCsv<
-    typeof PointTypeSchema
-  >(
-    '/incursion/imported/PointType 5a48b198234543278cf43279915248ce_all.csv',
-    PointTypeSchema,
-    (rowPointType) => {
-      return {
-        ...rowPointType,
-        maxPoints:
-          rowPointType.maxPoints && typeof rowPointType.maxPoints === 'string'
-            ? parseInt(rowPointType.maxPoints)
-            : null,
-      };
-    }
-  );
-
-  const subCategoriesByKey: Record<SubCategoryKey, SubCategory> =
-    await parseCsv<typeof SubCategorySchema>(
+  const [
+    categoriesByKey,
+    entitiesByKey,
+    pointTypesByKey,
+    subCategoriesByKey,
+    entityLevelsByKey,
+    appendicesByKey,
+    setting,
+  ] = await Promise.all([
+    parseCsv<typeof CategorySchema>(
+      '/incursion/imported/Categories 209902f319f64b48bc9cca434d7e449b_all.csv',
+      CategorySchema,
+      (rowCategory) => {
+        return {
+          ...rowCategory,
+          subCategories:
+            rowCategory.subCategories &&
+            typeof rowCategory.subCategories === 'string'
+              ? rowCategory.subCategories.split(',')
+              : [],
+          appendices:
+            rowCategory.appendices && typeof rowCategory.appendices === 'string'
+              ? rowCategory.appendices.split(',')
+              : [],
+        };
+      }
+    ),
+    parseCsv<typeof EntitySchema>(
+      '/incursion/imported/Entities ec7305ac1c814f7290ef03a1b62b6549_all.csv',
+      EntitySchema,
+      (rowEntity) => {
+        return {
+          ...rowEntity,
+          imageUrl: `/incursion/imported/${rowEntity.Image}`,
+          entityLevels:
+            rowEntity.entityLevels && typeof rowEntity.entityLevels === 'string'
+              ? rowEntity.entityLevels.split(',')
+              : [],
+        };
+      }
+    ),
+    parseCsv<typeof PointTypeSchema>(
+      '/incursion/imported/PointType 5a48b198234543278cf43279915248ce_all.csv',
+      PointTypeSchema,
+      (rowPointType) => {
+        return {
+          ...rowPointType,
+          maxPoints:
+            rowPointType.maxPoints && typeof rowPointType.maxPoints === 'string'
+              ? parseInt(rowPointType.maxPoints)
+              : null,
+        };
+      }
+    ),
+    parseCsv<typeof SubCategorySchema>(
       '/incursion/imported/Subcategories 74b3bfb500de4b19982b9aced3f3123d_all.csv',
       SubCategorySchema
-    );
+    ),
+    parseCsv<typeof EntityLevelSchema>(
+      '/incursion/imported/Entity Levels c6999b8546224bf58fcdcde6692517e0_all.csv',
+      EntityLevelSchema,
+      (entityLevel) => {
+        return {
+          ...entityLevel,
+          level:
+            entityLevel.level && typeof entityLevel.level === 'string'
+              ? parseInt(entityLevel.level)
+              : undefined,
+          pointCost:
+            entityLevel.pointCost && typeof entityLevel.pointCost === 'string'
+              ? entityLevel.pointCost === ''
+                ? undefined
+                : parseInt(entityLevel.pointCost)
+              : undefined,
+        };
+      }
+    ),
+    parseCsv<typeof AppendixSchema>(
+      '/incursion/imported/Appendices d3e0624b649b48e2970db9f9c83b3e27_all.csv',
+      AppendixSchema
+    ),
+    fetch(
+      '/incursion/imported/System & Setting 61b4b535ec174572a4419910929e7022.md'
+    ),
+  ]);
 
-  const entityLevelsByKey: Record<EntityLevelKey, EntityLevel> = await parseCsv<
-    typeof EntityLevelSchema
-  >(
-    '/incursion/imported/Entity Levels c6999b8546224bf58fcdcde6692517e0_all.csv',
-    EntityLevelSchema,
-    (entityLevel) => {
-      return {
-        ...entityLevel,
-        level:
-          entityLevel.level && typeof entityLevel.level === 'string'
-            ? parseInt(entityLevel.level)
-            : undefined,
-        pointCost:
-          entityLevel.pointCost && typeof entityLevel.pointCost === 'string'
-            ? entityLevel.pointCost === ''
-              ? undefined
-              : parseInt(entityLevel.pointCost)
-            : undefined,
-      };
-    }
-  );
-
-  const appendicesByKey: Record<AppendixKey, Appendix> = await parseCsv<
-    typeof AppendixSchema
-  >(
-    '/incursion/imported/Appendices d3e0624b649b48e2970db9f9c83b3e27_all.csv',
-    AppendixSchema
-  );
-
-  const setting = await fetch(
-    '/incursion/imported/System & Setting 61b4b535ec174572a4419910929e7022.md'
-  );
   let settingText = await setting.text();
 
   [settingText] = [settingText].map((text) =>
@@ -149,9 +143,9 @@ async function parseCsv<SchemaType extends ZodType<any, any, any>>(
       delimiter: ',',
       header: true,
       complete: async function (results) {
-        const resultsByKey = await reduce(
+        const selfPaths = reduce(
           results.data,
-          async (acc, result) => {
+          (acc, result) => {
             if (callback) result = callback(result);
             if (typeof result.Self !== 'string') {
               console.error('no self', result);
@@ -163,26 +157,37 @@ async function parseCsv<SchemaType extends ZodType<any, any, any>>(
               throw new Error('Cannot parse Self');
             }
             const pathToSelf = `${selfPaths[0].slice(1, -1)}.md`;
+            return [...acc, pathToSelf];
+          },
+          [] as string[]
+        );
+
+        const resultsByKey: Record<string, z.infer<SchemaType>> = {};
+        await Promise.all(
+          selfPaths.map(async (pathToSelf, index) => {
+            let result = results.data[index];
+            if (callback) result = callback(result);
+            if (typeof result.Self !== 'string') {
+              console.error('no self', result);
+              throw new Error('No Self');
+            }
+            console.log(pathToSelf, result);
             const description = await fetchMarkdown(pathToSelf);
+            const parsedResult = parser.parse({
+              ...result,
+              description,
+            });
+            if (!parsedResult.key) {
+              console.error('key not found', parsedResult);
+              throw new Error('key not found');
+            }
+            resultsByKey[parsedResult.key] = parsedResult;
             try {
-              const parsedResult = parser.parse({
-                ...result,
-                description,
-              });
-              if (!parsedResult.key) {
-                console.error('key not found', parsedResult);
-                throw new Error('key not found');
-              }
-              return {
-                ...(await acc),
-                [parsedResult.key]: parsedResult,
-              };
             } catch (err) {
               console.log('Error parsing', result);
               throw err;
             }
-          },
-          Promise.resolve({} as Record<string, z.infer<SchemaType>>)
+          })
         );
         resolve(resultsByKey);
       },
