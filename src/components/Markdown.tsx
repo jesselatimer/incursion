2d947ee0c5b7af3eb901e7f391003fc229af76a1
Jesse Latimer <jesselatimer@MacBook-Pro.local>
2024-10-12 18:03:29 -0700

    memoize markdown

--- src/components/Markdown.tsx
+++ src/components/Markdown.tsx
@@ -3,6 +3,7 @@ import { IN_WORLD_TERMS, CYOA_TERMS } from './../data/glossary';
 import GlossaryOverlay from './GlossaryOverlay';
 import * as mdx from '@mdx-js/mdx';
 import * as runtime from 'react/jsx-runtime';
+import { useMemo } from 'react';
 
 function Null() {
   return null;
@@ -18,36 +19,41 @@ function Markdown({
   children: React.ReactNode;
   className?: string;
 }) {
-  if (!children || typeof children !== 'string') return null;
-  console.log(IN_WORLD_TERMS_BY_PLURAL);
+  const Markdown = useMemo(() => {
+    if (!children || typeof children !== 'string') return null;
+    const markdownList = children.split(/([^a-zA-z])/);
+    const modifiedMarkdown = markdownList.map((str: string) => {
+      const inWorldDescription =
+        IN_WORLD_TERMS[str.toLowerCase() as keyof typeof IN_WORLD_TERMS]
+          ?.description ??
+        IN_WORLD_TERMS_BY_PLURAL[
+          str.toLowerCase() as keyof typeof IN_WORLD_TERMS
+        ]?.description;
+      const cyoaDescription =
+        CYOA_TERMS[str as keyof typeof CYOA_TERMS]?.description ??
+        CYOA_TERMS_BY_PLURAL[str as keyof typeof CYOA_TERMS]?.description;
+      if (inWorldDescription !== undefined) {
+        return `<GlossaryOverlay variant="in-world" str="${str}" description={\`${inWorldDescription}\`}>${str}</GlossaryOverlay>`;
+      } else if (cyoaDescription !== undefined) {
+        return `<GlossaryOverlay variant="cyoa" str="${str}" description={\`${cyoaDescription}\`}>${str}</GlossaryOverlay>`;
+      } else {
+        return str;
+      }
+    });
 
-  const markdownList = children.split(/([^a-zA-z])/);
-  const modifiedMarkdown = markdownList.map((str: string) => {
-    const inWorldDescription =
-      IN_WORLD_TERMS[str.toLowerCase() as keyof typeof IN_WORLD_TERMS]
-        ?.description ??
-      IN_WORLD_TERMS_BY_PLURAL[str.toLowerCase() as keyof typeof IN_WORLD_TERMS]
-        ?.description;
-    const cyoaDescription =
-      CYOA_TERMS[str as keyof typeof CYOA_TERMS]?.description ??
-      CYOA_TERMS_BY_PLURAL[str as keyof typeof CYOA_TERMS]?.description;
-    if (inWorldDescription !== undefined) {
-      return `<GlossaryOverlay variant="in-world" str="${str}" description={\`${inWorldDescription}\`}>${str}</GlossaryOverlay>`;
-    } else if (cyoaDescription !== undefined) {
-      return `<GlossaryOverlay variant="cyoa" str="${str}" description={\`${cyoaDescription}\`}>${str}</GlossaryOverlay>`;
-    } else {
-      return str;
-    }
-  });
+    const compiledMarkdown = mdx.compileSync(modifiedMarkdown.join(''), {
+      outputFormat: 'function-body',
+    });
 
-  const compiledMarkdown = mdx.compileSync(modifiedMarkdown.join(''), {
-    outputFormat: 'function-body',
-  });
+    const { default: Markdown } = mdx.runSync(String(compiledMarkdown), {
+      ...runtime,
+      Fragment: 'fragment',
+    });
 
-  const { default: Markdown } = mdx.runSync(String(compiledMarkdown), {
-    ...runtime,
-    Fragment: 'fragment',
-  });
+    return Markdown;
+  }, []);
+
+  if (!Markdown) return null;
 
   return (
     <div className={className}>
